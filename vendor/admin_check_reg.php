78cccef7d305aca8b9c0173bb29b0f37cfb65e06
Ziklson <etothezik@gmail.com>
2022-11-10 13:22:47 +0300

    start working on admin page

---
+++ vendor/admin_check_reg.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+require_once 'connect.php';
+
+
+$id = $_POST["autoservice_id"];
+if (empty($id)) {
+    echo "Что то не так";
+    print_r($_POST);
+} else {
+    $sql = "SELECT name_autoservice,email_autoservice,phone_autoservice,document
+    FROM Public.autoservice_in_check
+    WHERE autoservice_temp_id ='$id'";
+
+    $result = $pdo->query($sql)->fetch();
+    $_SESSION['autoservice_in_check'] = [
+        "id"   => $id,
+        "name" => $result['name_autoservice'],
+        "email" => $result['email_autoservice'],
+        "phone" => $result['phone_autoservice'],
+        "document" => $result['document']
+    ];
+    header("Location: ../admin_check_reg_page.php");
+}
--- vendor/path.php
+++ vendor/path.php
@@ -4,3 +4,6 @@ $path_vendor = "/careauto.ru/vendor/";
 $path_uploads_regular = "../uploads/regular"; // выйти из папки вендор и перейти в uploads
 $path_uploads_temp = "../uploads/temp/";
 $path_assets = "/careauto.ru/assets/";
+$path_admin_check_reg = "/careauto.ru/admin_check_reg.php";
+$path_admin_banlist = "/careauto.ru/admin_banlist.php";
+$path_admin_complaint = "/careauto.ru/admin_complaint.php";
